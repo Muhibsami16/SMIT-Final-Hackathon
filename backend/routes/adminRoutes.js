ba8376dab32ca1ba544617f01acaf205434e58ab
Muhib Sami <muhib.sami.pk@gmail.com>
2025-12-31 23:27:52 +0500

    Railway url fix

--- backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAdminStats } from '../controllers/adminController.js';
-import { protect, admin } from '../middlewares/authMiddleware.js';
-
-const router = express.Router();
-
-router.get('/stats', protect, admin, getAdminStats);
-
-export default router;
